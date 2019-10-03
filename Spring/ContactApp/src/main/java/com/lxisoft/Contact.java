5d9db3330d94dda9e3cb8a2ff53abb3e8e742862
Pavana <pavana.n.p@lxisoft.com>
2019-10-03 16:47:32 +0530

    IN-1031

---
+++ Spring/ContactApp/src/main/java/com/lxisoft/Contact.java
@@ -0,0 +1,19 @@
+package com.lxisoft;
+
+public class Contact {
+	private String name;
+	private String contactNumber;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+}
---
+++ Spring/ContactApp/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Contacts</h2>
+<form action="" method="post">
+Contact name<input type="text" name="name"><br><br>
+Contact number<input type="text" name="num"><br><br>
+<button>Submit</button>
+</form>
+</body>
+</html>
