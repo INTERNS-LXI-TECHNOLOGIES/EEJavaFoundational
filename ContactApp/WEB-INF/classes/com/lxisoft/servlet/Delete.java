6bb0d8b31ec820d0a746701e9eeaea6e3e23959a
unknown <abhijith.v.m@lxisoft.com>
2019-12-16 16:53:22 +0530

    IN-3688

---
+++ ContactApp/WEB-INF/classes/com/lxisoft/servlet/Delete.java
@@ -0,0 +1,8 @@
+package com.lxisoft.servlet;
+import java.util.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+import com.lxisoft.repository.*;
+import com.lxisoft.model.*;
+import com.lxisoft.sqlrepository.*;
