cd71e49408fce860350c0071a480f27cebb2e6f5
amritha <amritha.p.m@lxisoft.com>
2020-02-10 16:38:48 +0530

    http://jira.bytatech.com:8080/browse/IN-6145

---
+++ MockExam/src/com/lxisoft/Mockexam/Model.java
@@ -0,0 +1,23 @@
+public class Model
+{
+	private int qNo;
+	private String question;
+	public void setQuestion(String question)
+	{
+		this.question = question;
+	}
+	public String getQuestion()
+	{
+		return question;
+	}
+	public void setQuestionNumber(int qNo)
+	{
+		this.qNo=qNo;
+	}
+	public int getQuestionNumber()
+	{
+		return qNo;
+	}
+
+
+}
