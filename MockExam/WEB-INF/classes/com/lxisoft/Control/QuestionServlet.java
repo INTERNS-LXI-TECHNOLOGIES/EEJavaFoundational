9d06c116931bd88b4550775be07d0c9d44c8df87
amritha <amritha.p.m@lxisoft.com>
2020-02-14 13:17:09 +0530

    http://jira.bytatech.com:8080/browse/IN-6362

--- MockExam/WEB-INF/classes/com/lxisoft/Control/QuestionServlet.java
+++ MockExam/WEB-INF/classes/com/lxisoft/Control/QuestionServlet.java
@@ -5,21 +5,29 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import com.lxisoft.Model.ExamModel;
+import com.lxisoft.Repository.SqlRepository;
 public class QuestionServlet extends HttpServlet
 {
+    public ArrayList<ExamModel> emodels=new ArrayList<ExamModel>();
+    public SqlRepository sqlrepo=new SqlRepository();
     public void doGet(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException  
     {
-       // ArrayList<Question> questions=new ArrayList<Question>();
-    	/*SqlRepository sqlrepo = new SqlRepository(); 
-       	for(int i=0;i<questions.size();i++)
-       	{
-       		Question quest=new Question();
-       	}		*/
         PrintWriter out=response.getWriter();
-        out.println("deegghjj");
+      //  out.println("deegghjj");
+        response.setContentType("text/html");
+        emodels=sqlrepo.readFromDatabase(emodels);       
+        
+        for(int i=0;i<emodels.size();i++)
+        {
+           out.println("qNo"+emodels.get(i).getQuestionNumber());
+           out.println("question"+emodels.get(i).getQuestion());
+           out.println("opt 1"+emodels.get(i).getOpt1());
+           out.println("opt 2"+emodels.get(i).getOpt2());
+           out.println("opt 3"+emodels.get(i).getOpt3());
+           out.println("opt 4"+emodels.get(i).getOpt4());
+        }
+        
     }     
 }
 
-
- 
-       
--- MockExam/WEB-INF/classes/com/lxisoft/Repository/SqlRepository.java
+++ MockExam/WEB-INF/classes/com/lxisoft/Repository/SqlRepository.java
@@ -3,11 +3,12 @@ import java.util.*;
 import java.io.*;
 import java.sql.*;
 import com.lxisoft.Repository.SqlRepository;
+import com.lxisoft.Model.ExamModel;
 public class SqlRepository
 {
 	Connection con = null;
 	PreparedStatement ps = null;
-	//ResultSet rs=null;
+	ResultSet rs=null;
 	public void dataBaseConnectionEstablish()
 	{
 		if (con == null)
@@ -23,13 +24,13 @@ public class SqlRepository
 			}	
 		}	
 	}
-	/*public void createTable()
+	public void createTable()
 	{
 		try
 		{
-			databaseConnectionEstablish();
+			this.dataBaseConnectionEstablish();
 			String qry;
-			qry="create table if not exists mockexam(id int not null unique auto_increment,question varchar(250),opt1 varchar(25),opt2 varchar(25),opt3 varchar(25),opt4 varchar(25),ans varchar(25))";
+			qry="create table mockexam(id int,question varchar(250),opt1 varchar(25),opt2 varchar(25),opt3 varchar(25),opt4 varchar(25),ans varchar(25))";
 			ps=con.prepareStatement(qry);
 			ps.execute();
 		}
@@ -43,15 +44,48 @@ public class SqlRepository
  	{
  		try
  		{
- 			databaseConnectionEstablish();
+ 			this.dataBaseConnectionEstablish();
 			String qry="insert into mockexam(question,opt1,opt2,opt3,opt4,ans)values(?,?,?,?)";
-			qry=con.prepareStatement(qry);	
+			ps=con.prepareStatement(qry);	
 			ps.executeUpdate();
 	 	}
  		catch(Exception e)
  		{
  			System.out.println(e);
  		}
- 	}*/
-}
+ 	}
+ 	public ArrayList<ExamModel> readFromDatabase(ArrayList<ExamModel> emodels)
+	{
+		try
+		{
+			String qry = "select * from mockexam";
+			ps = con.prepareStatement(qry);
+			rs = ps.executeQuery(qry);
+			while(rs.next())
+			{
+				emodels.add(new ExamModel());
+				for(int i=0;i<emodels.size();i++)
+				{
+					if((emodels.get(i)).getQuestionNumber()==0)
+					{
+						emodels.get(i).setQuestionNumber(rs.getInt(1));
+						emodels.get(i).setQuestion(rs.getString(2));
+						emodels.get(i).setOpt1(rs.getString(3));
+						emodels.get(i).setOpt2(rs.getString(4));
+						emodels.get(i).setOpt3(rs.getString(5));
+						emodels.get(i).setOpt4(rs.getString(6));
+					}
+				}
+			}			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return emodels;
+	}
+		
+}	
 
+
+      
