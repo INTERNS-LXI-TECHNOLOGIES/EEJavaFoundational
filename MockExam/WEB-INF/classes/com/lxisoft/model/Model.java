82b8537ccdce9d38b1bd91f0d59e4bf145072870
sanfar <muhammed.sanfar.m@lxisoft.com>
2020-02-17 16:55:10 +0530

    http://jira.bytatech.com:8080/browse/IN-6433

---
+++ MockExam/WEB-INF/classes/com/lxisoft/model/Model.java
@@ -0,0 +1,7 @@
+package com.lxisoft.model;
+import java.sql.*;  
+import java.util.*;
+public class model
+{
+
+}
--- MockExam/WEB-INF/classes/com/lxisoft/sqlrepository/Sqlrepository.java
+++ MockExam/WEB-INF/classes/com/lxisoft/sqlrepository/Sqlrepository.java
@@ -1,6 +1,11 @@
 package com.lxisoft.sqlrepository;
+import java.sql.*;  
+import java.util.*;
 public class Sqlrepository
 {
+	Connection con=null;
+	PreparedStatement ps=null;
+
 	public void connection()
 	{
 	 try{
@@ -15,6 +20,13 @@ public class Sqlrepository
 			 	e.printStackTrace();
 			 	System.out.println(e);
 			} 
+	}
 
-	}  
+	public void readquestion()
+	{
+		connection();
+			 	String query="select question from mockexam where id=?";
+			 	ps.setInt(1);
+			 	ResultSet rs = st.executeQuery(query);
+
 }
