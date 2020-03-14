c22425ef404f4426ea7dd378c7b1dbe1cebae2cd
sanfar <muhammed.sanfar.m@lxisoft.com>
2020-03-14 15:53:38 +0530

    http://jira.bytatech.com:8080/browse/IN-7468

--- example/src/main/java/com/lxisoft/sqlrepository/Sqlrepository.java
+++ example/src/main/java/com/lxisoft/sqlrepository/Sqlrepository.java
@@ -132,5 +132,27 @@ public class Sqlrepository
 			} 
 
 	}
+	public void addUser()
+	{
+		questions.clear();
+		try{
+			connection();
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select * from users");
+			Model model=null;
+			while (rs.next())
+			{
+				model=new Model();
+				model.setId(rs.getInt("id"));
+				model.setQuestion(rs.getString("Question"));
+				model.setAnswer(rs.getString("Answers"));
+				model.setOption1(rs.getString("option1"));
+				model.setOption2(rs.getString("option2"));
+				model.setOption3(rs.getString("option3"));
+				questions.add(model);
+			}
+		}
+			
+	}
 
 }
---
+++ example/src/main/webapp/login.jsp
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML >
+<html>
+<head>
+<title>MOCK EXAM</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+	<div class="box">
+		<form >
+			<h2><font >Login</h2><br><br>
+			<div class="inputbox">
+				<label >UserId</label>
+  				<input type="text" name="UserId">
+  				
+  				</div>
+  			<div class="inputbox">
+  				<label >Password</label>
+  				<input type="text" name="Password">	
+  			</div>
+			</font>
+			
+		</form>
+<form >
+	<div style="text-align:center;">
+		<button formaction="login" type="submit" >submit</button>
+	</div>
+	</div>
+</form>
+</body>
+</html>
