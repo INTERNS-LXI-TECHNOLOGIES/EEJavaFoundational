a224ba5840a2aeaa8bbc67b9e8f26a03d1aefaa6
Akhilesh-Narayanan <akhileshnarayanan.m.p@lxisoft.com>
2020-07-23 14:47:28 +0530

    http://jira.bytatech.com:8080/browse/IN-8466

--- MockExam/New folder/src/com/lxisoft/controller/AddQuestionServlet.java
+++ MockExam/New folder/src/com/lxisoft/controller/AddQuestionServlet.java
@@ -1,24 +1,31 @@
 package com.lxisoft.controller;
 import java.io.IOException;
-
 import com.lxisoft.model.*;
 import com.lxisoft.repository.*;
-
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
-
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 public class AddQuestionServlet extends HttpServlet
 {
-	private Question question;
-	private DatabaseRepo db;	
+	private Question question=new Question();
+	private DatabaseRepo db = new DatabaseRepo();;	
 	public void doGet(HttpServletRequest request , HttpServletResponse response) throws ServletException, IOException
 	{
+		try
+		{
 		question.setQuestion(request.getParameter("question"));
 		question.setAnswer(request.getParameter("answere"));
 		question.setOption1(request.getParameter("opt1"));
 		question.setOption2(request.getParameter("opt2"));
 		question.setOption3(request.getParameter("opt3"));
 		db.addToDatabase(question);
+		response.sendRedirect("index.html");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 }
