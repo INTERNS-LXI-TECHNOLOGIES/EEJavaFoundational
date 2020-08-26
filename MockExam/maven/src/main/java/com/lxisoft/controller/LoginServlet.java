f4367a81c111b9351fda617f63b44e533de86904
Akhilesh-Narayanan <akhileshnarayanan.m.p@lxisoft.com>
2020-08-26 17:24:17 +0530

    http://jira.bytatech.com:8080/browse/IN-9808

--- MockExam/maven/src/main/java/com/lxisoft/controller/LoginServlet.java
+++ MockExam/maven/src/main/java/com/lxisoft/controller/LoginServlet.java
@@ -7,11 +7,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginServlet extends HttpServlet	
 {
 	public void doGet(HttpServletRequest request ,HttpServletResponse response) throws ServletException, IOException
 	{
+		HttpSession session = request.getSession();
+		session.invalidate();
 		PrintWriter pw = response.getWriter();
 		String username = request.getRemoteUser();
 		//pw.println("mark = "+username );
