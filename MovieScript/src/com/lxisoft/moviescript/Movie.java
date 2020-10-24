6f675865576148f694748f1df4656ae535b0f18c
Aswathy <aswathy.m@lxisoft.com>
2020-10-24 16:15:20 +0530

    http://jira.bytatech.com:8080/browse/IN-11602

--- MovieScript/src/com/lxisoft/moviescript/Movie.java
+++ MovieScript/src/com/lxisoft/moviescript/Movie.java
@@ -16,6 +16,7 @@ public class Movie{
 	Comedian comedian=new Comedian();
     FileReppo filereppo=new FileReppo();
     Scanner scanner=new Scanner(System.in);
+    String dialogue;
 
 	Actors actors=new Actors();
 
@@ -51,17 +52,99 @@ public class Movie{
         if(choice==0){
 
 
-        System.out.println("\n A RANDOM SCENE");
+       
+        this.adminFunctions();
+
+         System.out.println("\n A RANDOM SCENE");
         System.out.println("************************************************************************************************");
+        
+
         comedian.setcomedianDialoges();
         filereppo.fileCreation();
+     
         
 
 
             }
+        }
+        public void adminFunctions(){
+        System.out.println("**********************************");
+        System.out.println("\n \n  ***THE CURRENT Comedian SCRIPT IS***");
+       
+        boolean quit=false;
+
+         int choice=0;
+        do
+        {
+
+            System.out.println("\n \n ***ENTER YOUR DESIRED OPERATION TO PERFORM***");
+            System.out.println("1.  ADD");
+            System.out.println("2.  DELETE");
+            System.out.println("3.  UPDATE");
+            
+            System.out.println("0.  TO EXIT");
+
+            System.out.println("Enter your choice");
+             choice=scanner.nextInt();
 
 
-    }
+            
+            
+        
+                if(choice==1)
+                {
+                this.addDynamically();
+                }
+                if(choice==2)
+                {
+                this.removeDynamically();
+                }
+                if(choice==3)
+                {
+                this.modifyNameDynamically();
+                }
+                
+                else if (choice==0)
+                {
+                System.out.println(" ");
+                }
+            }
+            while(choice>0);
+      
+                
+            
+
+            
+            }
+
+            public void addDynamically(){
+
+
+            System.out.println("Enter the nextLine" );
+             dialogue=scanner.next();
+
+
+          
+            comedian.comedianScript.add(dialogue);
+           
+            }
+             public void removeDynamically(){
+            System.out.println("Enter the index Number of item to be deleted");
+            int indexnumber=scanner.nextInt();
+
+            comedian.comedianScript.remove(indexnumber);
+            }
+
+            
+            
+             public void  modifyNameDynamically(){
+                System.out.println("Enter the index number to be modified");
+                int indexnumber=scanner.nextInt();
+                System.out.println("Enter the new name");
+                String newDialouge=scanner.next();
+               // comedian.comedianScript[indexnumber].set(newDialouge);
+            }
+    
 
    
 
