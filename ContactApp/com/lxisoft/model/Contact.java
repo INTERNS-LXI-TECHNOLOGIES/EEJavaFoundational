edd98aed9fc4b254ea6bba9fadcc5d7a619b00be
[SAHADH] <[sahadh.k.s@lxisoft.com]>
2022-03-09 17:27:22 +0530

    http://34.87.37.122:8080/browse/IN-20870

--- ContactApp/com/lxisoft/model/Contact.java
+++ ContactApp/com/lxisoft/model/Contact.java
@@ -1,32 +1,47 @@
-package com.lxisoft;
-import java.sql.*;
-public class Contact{
-    static Connection con = null;
-    static PreparedStatement ps = null;
-    static Statement st = null;
-    static ResultSet rs = null;
-     public static ResultSet retriveData(){
-     
-     try{
-          
-          Class.forName("com.mysql.cj.jdbc.Driver"); 
-          con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lxisoft","root","root"); 
-          st = con.createStatement(); 
-          rs = st.executeQuery("select * from contacts "); 
-          while(!rs.isLast() && rs.next()) 
-          {
-               System.out.println("First_Name : " +rs.getString(1)+ "\n Last_Name : "+rs.getString(2)+ "\n PH_Number : "+rs.getString(3)+ "\n Mail_ID : "+ rs.getString(4));
-           } con.close();
-    st.close();
-    return rs;
-}catch(Exception e){
-     System.out.println(e);
-     e.printStackTrace();
-    }
-    return rs;
-}
-public static void main(String[] args) {
-     retriveData();
-}
+package com.lxisoft.model;
 
+public class Contact {
+     private String firstName,lastName,phNumber,email;
+	private int id;
+
+	public void setFirstName(String firstName)
+	{
+		this.firstName= firstName;
+	}      
+	public String getFirstName()
+	{
+		return this.firstName;
+	}
+	public void setLastName(String lastName)
+	{
+		this.lastName= lastName;
+	}      
+	public String getLastname()
+	{
+		return this.lastName;
+	}
+	public void setPhNumber(String number)
+	{
+		this.phNumber= number; 
+	}
+	public String getPhNumber()
+	{
+		return this.phNumber;
+	}
+     public void setEmail(String mail)
+	{
+		this.email= mail; 
+	}
+	public String getEmail()
+	{
+		return this.email;
+	}
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	public int getId()
+	{
+		return this.id;
+	}
 }
