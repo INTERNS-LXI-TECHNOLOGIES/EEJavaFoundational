9db693e247ec06b77e405b6535a7493a62fcf1f6
ShabnaSirajudeen <shabna.sirajudeen@lxisoft.com>
2022-03-15 17:37:47 +1100

    http://34.87.37.122:8080/browse/IN-20967

--- UsedCarShowroomApp/src/main/java/com/lxisoft/carshowroom/servlet/ControllerServlet.java
+++ UsedCarShowroomApp/src/main/java/com/lxisoft/carshowroom/servlet/ControllerServlet.java
@@ -21,21 +21,33 @@ public class ControllerServlet extends HttpServlet {
 		String jdbcURL = getServletContext().getInitParameter("jdbcURL");
 		String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
 		String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
-
 		carDAO = new CarDAO(jdbcURL, jdbcUsername, jdbcPassword);
-
 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getServletPath();
 
 		try {
 			switch (action) {
-			case "/create":
-				createCar(request, response);
-				break;
 			case "/insert":
 				insertCar(request, response);
+				break;
+			case "/update":
+				updateCar(request, response);
+				break;
+			}
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getServletPath();
+		
+		try {
+			switch (action) {
+			case "/create":
+				createCar(request, response);
 				break;
 			case "/delete":
 				deleteCar(request, response);
@@ -43,11 +55,11 @@ public class ControllerServlet extends HttpServlet {
 			case "/edit":
 				editCar(request, response);
 				break;
-			case "/update":
-				updateCar(request, response);
+			case "/home":
+				home(request, response);
 				break;
 			default:
-				home(request, response);
+				response.sendRedirect("home");
 				break;
 			}
 		} catch (SQLException ex) {
@@ -60,20 +72,66 @@ public class ControllerServlet extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 
-	private void insertCar(HttpServletRequest request, HttpServletResponse response) {
-		
+	private void insertCar(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		String manufacturer = request.getParameter("manufacturer");
+		String model = request.getParameter("model");
+		String variant = request.getParameter("variant");
+		int year = Integer.parseInt(request.getParameter("year"));
+		int totalKilometers = Integer.parseInt(request.getParameter("totalKilometers"));
+		int expectedPrice = Integer.parseInt(request.getParameter("expectedPrice"));
+		String otherDetails = request.getParameter("otherDetails");
+
+		Car newCar = new Car();
+		newCar.setManufacturer(manufacturer);
+		newCar.setModel(model);
+		newCar.setVariant(variant);
+		newCar.setYear(year);
+		newCar.setTotalKilometers(totalKilometers);
+		newCar.setExpectedPrice(expectedPrice);
+		newCar.setOtherDetails(otherDetails);
+		carDAO.insertCar(newCar);
+		response.sendRedirect("home");
 	}
 
-	private void deleteCar(HttpServletRequest request, HttpServletResponse response) {
-		
+	private void deleteCar(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		int carId = Integer.parseInt(request.getParameter("carId"));
+
+		Car car = new Car();
+		car.setCarId(carId);
+		carDAO.deleteCar(car);
+		response.sendRedirect("home");
 	}
 
-	private void editCar(HttpServletRequest request, HttpServletResponse response) {
-		
+	private void editCar(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+		int carId = Integer.parseInt(request.getParameter("carId"));
+		Car existingCar = carDAO.getCar(carId);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("createOrUpadateCar.jsp");
+		request.setAttribute("car", existingCar);
+		dispatcher.forward(request, response);
+
 	}
 
-	private void updateCar(HttpServletRequest request, HttpServletResponse response) {
-		
+	private void updateCar(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		int carId = Integer.parseInt(request.getParameter("carId"));
+		String manufacturer = request.getParameter("manufacturer");
+		String model = request.getParameter("model");
+		String variant = request.getParameter("variant");
+		int year = Integer.parseInt(request.getParameter("year"));
+		int totalKilometers = Integer.parseInt(request.getParameter("totalKilometers"));
+		int expectedPrice = Integer.parseInt(request.getParameter("expectedPrice"));
+		String otherDetails = request.getParameter("otherDetails");
+
+		Car car = new Car();
+		car.setCarId(carId);
+		car.setManufacturer(manufacturer);
+		car.setModel(model);
+		car.setVariant(variant);
+		car.setYear(year);
+		car.setTotalKilometers(totalKilometers);
+		car.setExpectedPrice(expectedPrice);
+		car.setOtherDetails(otherDetails);
+		carDAO.updateCar(car);
+		response.sendRedirect("home");
 	}
 
 	private void home(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
