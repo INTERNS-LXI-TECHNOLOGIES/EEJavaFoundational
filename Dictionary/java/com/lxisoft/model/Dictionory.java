a1f1e21ce5fbec6220c1dd761da1d6089be1a342
kasujith <sujith.k.a@lxisoft.com>
2022-05-04 18:54:41 +0530

    MVC_PATTERN

---
+++ Dictionary/java/com/lxisoft/model/Dictionory.java
@@ -0,0 +1,22 @@
+package com.lxisoft.model;
+
+public class Dictionory {
+    private String words;
+    private String meaning;
+
+    public String getWords() {
+        return words;
+    }
+
+    public void setWords(String words) {
+        this.words = words;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning =meaning;
+    }
+}
