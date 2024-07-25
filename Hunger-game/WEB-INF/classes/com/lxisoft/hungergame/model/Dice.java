1927b2a170d1dbb46cd43a69ac204c9a82bb2e66
laalu10 <lalu000046@gmail.com>
2024-07-25 12:01:25 +0530

    hungergame compleated

---
+++ Hunger-game/WEB-INF/classes/com/lxisoft/hungergame/model/Dice.java
@@ -0,0 +1,25 @@
+package com.lxisoft.hungergame.model;
+
+public class Dice
+{
+	private int result ;
+
+
+
+	public int getResult() {
+		return result;
+	}
+	public void setResult(int result) {
+		this.result = result;
+	}
+	
+	
+	
+	
+	
+	public int roll()
+	{
+	 	result = (int) (Math.random()*6)+1;
+		return result;
+	}
+}
